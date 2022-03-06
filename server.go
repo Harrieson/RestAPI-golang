29cc781887992f6296e2005ffbe7877f35018124
Harrieson <harriemburu@gmail.com>
2022-03-06 17:52:19 +0300

    Golang API without dependencies

--- server.go
+++ server.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	// "crypto/rand"
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
+	"math/rand"
 	"net/http"
+	"os"
+	"strings"
 	"sync"
+	"time"
 )
 
 type Coaster struct {
@@ -55,36 +61,127 @@ func (h *coasterHandlers) get(w http.ResponseWriter, r *http.Request) {
 	w.Write(jsonBytes)
 }
 
+func (h *coasterHandlers) getRandomCoaster(w http.ResponseWriter, r *http.Request) {
+	ids := make([]string, len(h.store))
+	h.Lock()
+	i := 0
+	for id := range h.store {
+		ids[i] = id
+		i++
+	}
+	defer h.Unlock()
+
+	var target string
+	if len(ids) == 0 {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	} else if len(ids) == 1 {
+		target = ids[0]
+	} else {
+		rand.Seed(time.Now().UnixNano())
+		target = ids[rand.Intn(len(ids))]
+	}
+	// fmt.Println(target)
+
+	w.Header().Add("location", fmt.Sprintf("/coasters/%s", target))
+	w.WriteHeader(http.StatusFound)
+}
+func (h *coasterHandlers) getCoaster(w http.ResponseWriter, r *http.Request) {
+	parts := strings.Split(r.URL.String(), "/")
+	if len(parts) != 3 {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	if parts[2] == "random" {
+		h.getRandomCoaster(w, r)
+		return
+	}
+	// fmt.Println(parts[2])
+	h.Lock()
+	coaster, ok := h.store[parts[2]]
+	h.Unlock()
+	if !ok {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	jsonBytes, err := json.Marshal(coaster)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+	}
+
+	w.Header().Add("content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonBytes)
+}
+
 func (h *coasterHandlers) post(w http.ResponseWriter, r *http.Request) {
 	bodyBytes, err := ioutil.ReadAll(r.Body)
 	defer r.Body.Close()
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		w.Write([]byte(err.Error()))
+		return
 	}
 
+	ct := r.Header.Get("Content-type")
+	if ct != "application/json" {
+		w.WriteHeader(http.StatusUnsupportedMediaType)
+		w.Write([]byte(fmt.Sprintf("need Content-type 'application/json', but got '%s'", ct)))
+		return
+	}
+	var coaster Coaster
+	err = json.Unmarshal(bodyBytes, &coaster)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	coaster.ID = fmt.Sprintf("%d", time.Now().UnixNano())
+
 	h.Lock()
+	h.store[coaster.ID] = coaster
 	defer h.Unlock()
 }
 
 func newCoasterHandlers() *coasterHandlers {
 	return &coasterHandlers{
-		store: map[string]Coaster{
-			"id1": Coaster{
-				Name:         "Fury Road",
-				Height:       99,
-				ID:           "id1",
-				InPark:       "Carowinds",
-				Manufacturer: "B+M",
-			},
-		},
+		store: map[string]Coaster{},
 	}
 }
 
-func main() {
+type adminPortal struct {
+	password string
+}
 
+func newAdminPortal() *adminPortal {
+	password := os.Getenv("ADMIN_PASSWORD")
+	if password == "" {
+		panic("required env variable ADMIN_PASSWORD not set")
+	}
+	return &adminPortal{password: password}
+}
+
+func (a adminPortal) handler(w http.ResponseWriter, r *http.Request) {
+	user, pass, ok := r.BasicAuth()
+	if !ok || user != "admin" || pass != a.password {
+		w.WriteHeader(http.StatusUnauthorized)
+		w.Write([]byte("402 - Unauthorized"))
+		return
+	}
+
+	w.Write([]byte("<html><h1>Super secret admin portal</h1></html>"))
+}
+
+func main() {
+	admin := newAdminPortal()
 	coasterHandlers := newCoasterHandlers()
 	http.HandleFunc("/coasters", coasterHandlers.coasters)
+	http.HandleFunc("/coasters/", coasterHandlers.getCoaster)
+	http.HandleFunc("/admin", admin.handler)
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		panic(err)
